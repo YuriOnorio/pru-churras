017ae5e013d42ca8d3f82019136decca0003f1f4
YuriOnorio <yuri.onorio@etec.sp.gov.br>
2023-03-10 10:45:01 -0300

    .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,57 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  value = 1;
+
+  btn1 = 1;
+  btn2 = 3;
+  btn3 = 6;
+  btn4 = 9;
+  btn5 = 12;
+
+  point = 0;
+  point2 = 0
+
   constructor() {}
+
+  btnAdd1() {
+    this.value = this.btn1;
+  }
+
+  btnAdd2() {
+    this.value = this.btn2;
+  }
+
+  btnAdd3() {
+    this.value = this.btn3;
+  }
+
+  btnAdd4() {
+    this.value = this.btn4;
+  }
+
+  btnAdd5() {
+    this.value = this.btn5;
+  }
+
+  addValue() {
+    this.point += this.value;
+  }
+
+  addValue2() {
+    this.point2 += this.value;
+  }
+
+  removeValue() {
+    this.point -= this.value;
+  }
+
+  removeValue2() {
+    this.point2 -= this.value;
+  }
+
+  clean() {
+    console.log("foi")
+  }
 
 }
