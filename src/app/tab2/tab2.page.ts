860fbae332e7ca684e7df04d962dbe512eac4859
YuriOnorio <yronorio@gmail.com>
2023-03-17 12:12:58 -0300

    .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,55 +10,108 @@ export class Tab2Page {
 
   valor = 1;
 
-  pontuacao1 = 1;
-  pontuacao2 = 3;
-  pontuacao3 = 6;
-  pontuacao4 = 9;
-  pontuacao5 = 12;
+  partida1 = 0;
+  partida2 = 0;
 
   pontotime1 = 0;
   pontotime2 = 0
 
-  constructor() {}
+  constructor(
+    private toastController: ToastController,
+    private alertController: AlertController
+  ) {}
 
   pontuacaotime1() {
-    this.valor = this.pontuacao1;
+    this.valor = 1;
   }
 
   pontuacaotime2() {
-    this.valor = this.pontuacao2;
+    this.valor = 3;
   }
 
   pontuacaotime3() {
-    this.valor = this.pontuacao3;
+    this.valor = 6;
   }
 
   pontuacaotime4() {
-    this.valor = this.pontuacao4;
+    this.valor = 9;
   }
 
   pontuacaotime5() {
-    this.valor = this.pontuacao5;
+    this.valor = 12;
   }
 
-  adicionaValor() {
-    this.pontotime1 += this.valor;
+  async valorpartida() {
+    this.valor += this.valor;
+    if (this.valor >= 12) {
+      this.partida1 += 1;
+      this.pontotime1 = 0;
+      this.pontotime2 = 0;
+
+      let time1 = 'Time 1';
+
+      const alert = await this.alertController.create({
+        header: time1,
+        message: 'Ganhou a Partida!',
+        buttons: ['Continuar'],
+      });
+
+      await alert.present();
+    }
+
+    this.valor = 1;
   }
 
-  adicionaValor2() {
+  async valorpartida2() {
     this.pontotime2 += this.valor;
+    if (this.pontotime2 >= 12) {
+      this.partida2 += 1;
+      this.pontotime1 = 0;
+      this.pontotime2 = 0;
+
+      let time2 = 'Time 2';
+
+      const alert = await this.alertController.create({
+        header: time2,
+        message: 'Ganhou a Partida!',
+        buttons: ['Continuar'],
+      });
+
+      await alert.present();
+    }
+
+    this.valor = 1;
   }
 
-  removeValue() {
+  removervalor() {
     this.pontotime1 -= this.valor;
+    this.pontotime1 = this.pontotime1 < 0 ? 0 : this.pontotime1;
+
+    this.valor = 1;
   }
 
-  removeValue2() {
+  removervalor2() {
     this.pontotime2 -= this.valor;
+    this.pontotime2 = this.pontotime1 < 0 ? 0 : this.pontotime1;
+
+    this.valor = 1;
   }
 
-  clean() {
-    console.log("foi")
+  async cleanToast(position: 'bottom') {
+    this.valor = 1;
+    this.pontotime1 = 0;
+    this.pontotime2 = 0;
+    this.partida1 = 0;
+    this.partida2 = 0;
+
+    const toast = await this.toastController.create({
+      message: 'Pontuação zerada',
+      duration: 1500,
+      position: position
+    });
+
+    await toast.present();
   }
 
 }
+
